f10e3263754ecdfff0134f6a2fe9746b0ed361fc
Jhon Anderson Galeano Santillana <andersongs.474@gmail.com>
2025-07-27 17:41:11 -0500

    puestos de botones desde el root, cambiados a administrativos y estudiantes ingresando lo de las clases virtuales

--- src/pages/avancemosAdmin.jsx
+++ src/pages/avancemosAdmin.jsx
@@ -9,18 +9,12 @@ export default function Home() {
       className="bg-fixed bg-center bg-cover min-h-screen"
       style={{ backgroundImage: "url('/FotoCoorporativa.jpeg')" }}
     >
-      <div className="grid grid-cols-2 gap-16 max-h-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur bg-white/30 w-2/3 h-2/3 rounded-xl p-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:grid-rows-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur bg-white/30 w-2/3 h-2/3 rounded-xl p-8">
         <button
           className="bg-azul text-white text-xl font-bold px-4 hover:bg-white hover:text-azul cursor-pointer rounded-xl"
           onClick={() => router.push("/avancemosEstudiantes")}
         >
-          Estudiantes
-        </button>
-        <button
-          className="bg-azul text-white text-xl font-bold px-4 hover:bg-white hover:text-azul cursor-pointer rounded-xl"
-          onClick={() => router.push("avancemosAdmin")}
-        >
-          Administrativos
+          Sistema de inventarios
         </button>
       </div>
     </div>
---
+++ src/pages/avancemosEstudiantes.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <div
+      className="bg-fixed bg-center bg-cover min-h-screen"
+      style={{ backgroundImage: "url('/FotoCoorporativa.jpeg')" }}
+    >
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:grid-rows-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur bg-white/30 w-2/3 h-2/3 rounded-xl p-8">
+        <button
+          className="bg-azul text-white text-xl font-bold px-4 hover:bg-white hover:text-azul cursor-pointer rounded-xl"
+          onClick={() => router.push("/avancemosDigital")}
+        >
+          Clases virtuales
+        </button>
+      </div>
+    </div>
+  );
+}
