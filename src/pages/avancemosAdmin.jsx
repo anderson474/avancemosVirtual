fdac4b0c7eca2d4ae5ab75353f7e12b7dfe8ed87
Jhon Anderson Galeano Santillana <andersongs.474@gmail.com>
2025-07-29 14:43:17 -0500

    actualizacion de la avancemos digital con la puesta de inventarios junto

--- src/pages/avancemosAdmin.jsx
+++ src/pages/avancemosAdmin.jsx
@@ -10,12 +10,12 @@ export default function Home() {
       style={{ backgroundImage: "url('/FotoCoorporativa.jpeg')" }}
     >
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:grid-rows-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur bg-white/30 w-2/3 h-2/3 rounded-xl p-8">
-        <button
-          className="bg-azul text-white text-xl font-bold px-4 hover:bg-white hover:text-azul cursor-pointer rounded-xl"
-          onClick={() => router.push("/avancemosEstudiantes")}
+        <a
+          href="https://inventarios.avancemos.edu.co/"
+          className="flex items-center justify-center bg-azul text-white text-center text-xl font-bold px-4 hover:bg-white hover:text-azul cursor-pointer rounded-xl"
         >
           Sistema de inventarios
-        </button>
+        </a>
       </div>
     </div>
   );
